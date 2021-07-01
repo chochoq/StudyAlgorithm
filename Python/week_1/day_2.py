858b4ebdb587af07532d601bda48ef3a6a6a7427
chochoq <aubepe@gmail.com>
2021-07-01 20:08:03 +0900

    (add) day2

---
+++ Python/week_1/day_2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Q.  다음과 같은 문자열을 입력받았을 때, 어떤 알파벳이 가장 많이 포함되어 있는지 반환하시오
+
+# "hello my name is sparta"
+
+def find_alphabet_array(string):
+    alphabet_array = [0]*26
+
+    return alphabet_array
+
+
+result = find_alphabet_array
+
+print("정답 = a 현재 풀이 값 =")
+print(result("Hello my name is sparta"))
+print("정답 = a 현재 풀이 값 =")
+print(result("Sparta coding club"))
+print("정답 = s 현재 풀이 값 =")
+print(result("best of best sparta"))
