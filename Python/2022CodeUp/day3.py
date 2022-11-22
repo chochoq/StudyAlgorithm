3d3624f42915d34a37a9109295b44ced4911bc79
SaeByeok <72794550+chochoq@users.noreply.github.com>
2022-11-22 06:51:11 +0000

    코드업 기초 100

---
+++ Python/2022CodeUp/day3.py
@@ -0,0 +1,19 @@
+# 6024 : [기초-입출력] 단어 2개 입력받아 이어 붙이기(설명)
+a, b = input().split(' ')
+c = a+b
+print(c)
+# hello world
+
+
+# 6025 : [기초-값변환] 정수 2개 입력받아 합 계산하기(설명)
+a, b = input().split()
+c = int(a)+int(b)
+print(c)
+
+
+# 6026 : [기초-값변환] 실수 2개 입력받아 합 계산하기(설명)
+
+a = input()
+b = input()
+c = float(a)+float(b)
+print(c)
