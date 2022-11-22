44b67dfe12763c04ecbc9839d234c5f6edec402b
SaeByeok <72794550+chochoq@users.noreply.github.com>
2022-11-22 06:38:46 +0000

    코드업 기초100제

--- Python/2022CodeUp/day2.py
+++ Python/2022CodeUp/day2.py
@@ -6,3 +6,22 @@ print('print("Hello\\nWorld")')
 # 문자 1개 입력받아 그대로 출력하기
 inputString = input()
 print(inputString)
+
+
+#6018 : [기초-입출력] 시간 입력받아 그대로 출력하기(설명)(py)
+
+a, b = input().split(':')
+print(a, b, sep=':')
+
+
+# 6020 : [기초-입출력] 주민번호 입력받아 형태 바꿔 출력하기
+num, num1 = input().split('-')
+print(num, num1, sep='')
+
+# 6021 : [기초-입출력] 단어 1개 입력받아 나누어 출력하기(설명)(py)
+s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
